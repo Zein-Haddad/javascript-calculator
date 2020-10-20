9c66d1e52e366d6aff0b8d6bac96aa500a86138d
Zein-Haddad <zeinhaddad02@gmail.com>
2020-10-20 10:56:54 +0700

    Done part of UI

---
+++ js/math.js
@@ -0,0 +1,39 @@
+/*
+
+Make 2 variable, num and operator, num is an array consisting of 2 items,
+operator is filled with arithmetic function below (this happens when user press operator button)
+
+when user press num btn:
+  if operator is null:
+    modify num[0]
+  else:
+    modify num[1]
+
+when user press equal btn:
+  operate(function, num[0], num[1])
+
+*/
+
+function add (a, b) {
+  return a + b;
+}
+
+function subtract (a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+function modulus (a, b) {
+  return a % b;
+}
+
+function operate (operator, a, b) {
+  return operator(a, b);
+}
